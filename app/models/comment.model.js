9affa3e7ed2a5273940f712122b152cbbd5686ce
bychrisme <christianmeli81@gmail.com>
2020-08-07 21:56:52 +0100

    start using relation one to many

---
+++ app/models/comment.model.js
@@ -0,0 +1,14 @@
+const commentModel = (sequelize, Sequelize) => {
+    const Comment = sequelize.define("comment", {
+      name: {
+        type: Sequelize.STRING
+      },
+      text: {
+        type: Sequelize.STRING
+      }
+    });
+  
+    return Comment;
+  };
+  
+  module.exports = commentModel;
